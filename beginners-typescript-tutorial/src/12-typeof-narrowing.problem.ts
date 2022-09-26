42ff1f01a8d32c653712d7ce0a4b64efff899828
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-26 19:46:45 -0400

    Complete beginner tutorial

---
+++ beginners-typescript-tutorial/src/12-typeof-narrowing.problem.ts
@@ -0,0 +1,13 @@
+import { expect, it } from 'vitest';
+
+const coerceAmount = (amount: number | { amount: number }) => {
+  return typeof amount === 'number' ? amount : amount.amount;
+};
+
+it('Should return the amount when passed an object', () => {
+  expect(coerceAmount({ amount: 20 })).toEqual(20);
+});
+
+it('Should return the amount when passed a number', () => {
+  expect(coerceAmount(20)).toEqual(20);
+});
