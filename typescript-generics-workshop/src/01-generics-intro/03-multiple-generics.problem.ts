0d2479951dac23af8375e33d0bb97cc4b64a62c8
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-30 18:22:42 -0400

    Finish ex 03

--- typescript-generics-workshop/src/01-generics-intro/03-multiple-generics.problem.ts
+++ typescript-generics-workshop/src/01-generics-intro/03-multiple-generics.problem.ts
@@ -1,13 +1,13 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
-const returnBothOfWhatIPassIn = (a: unknown, b: unknown) => {};
+const returnBothOfWhatIPassIn = <T, U>(a: T, b: U) => ({ a, b });
 
-it("Should return a tuple of the arguments you pass", () => {
-  const result = returnBothOfWhatIPassIn("a", 1);
+it('Should return a tuple of the arguments you pass', () => {
+  const result = returnBothOfWhatIPassIn('a', 1);
 
   expect(result).toEqual({
-    a: "a",
+    a: 'a',
     b: 1,
   });
 
