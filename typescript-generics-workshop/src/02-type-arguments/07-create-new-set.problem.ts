b36c8f18232e912e0a3f99b374cda5e8f010ea22
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-30 18:48:55 -0400

    Finish ex 07 to 11

--- typescript-generics-workshop/src/02-type-arguments/07-create-new-set.problem.ts
+++ typescript-generics-workshop/src/02-type-arguments/07-create-new-set.problem.ts
@@ -1,7 +1,7 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-export const createSet = () => {
-  return new Set();
+export const createSet = <T>() => {
+  return new Set<T>();
 };
 
 const stringSet = createSet<string>();
@@ -11,5 +11,5 @@ const unknownSet = createSet();
 type tests = [
   Expect<Equal<typeof stringSet, Set<string>>>,
   Expect<Equal<typeof numberSet, Set<number>>>,
-  Expect<Equal<typeof unknownSet, Set<unknown>>>,
+  Expect<Equal<typeof unknownSet, Set<unknown>>>
 ];
--- typescript-generics-workshop/src/02-type-arguments/09-generics-in-type-arguments-in-arguments.problem.ts
+++ typescript-generics-workshop/src/02-type-arguments/09-generics-in-type-arguments-in-arguments.problem.ts
@@ -1,5 +1,5 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest';
+import { Equal, Expect } from '../helpers/type-utils';
 
 export class Component<TProps> {
   private props: TProps;
@@ -11,9 +11,11 @@ export class Component<TProps> {
   getProps = () => this.props;
 }
 
-const cloneComponent = (component: unknown) => {};
+const cloneComponent = <TProps>(component: Component<TProps>) => {
+  return new Component(component.getProps());
+};
 
-it("Should clone the props from a passed-in Component", () => {
+it('Should clone the props from a passed-in Component', () => {
   const component = new Component({ a: 1, b: 2, c: 3 });
 
   const clonedComponent = cloneComponent(component);
@@ -22,7 +24,5 @@ it("Should clone the props from a passed-in Component", () => {
 
   expect(result).toEqual({ a: 1, b: 2, c: 3 });
 
-  type tests = [
-    Expect<Equal<typeof result, { a: number; b: number; c: number }>>,
-  ];
+  type tests = [Expect<Equal<typeof result, { a: number; b: number; c: number }>>];
 });
